429833a5b399c4c5c8630f0ea6a725a851d25033
ggche <ggaleotecheca@gmail.com>
2025-09-21 12:30:00 +0200

    update setup.tcl innovus

--- pnr/setup.tcl
+++ pnr/setup.tcl
@@ -57,7 +57,8 @@ set vars(script_root)  [pwd]/SCRIPTS
 # vars(def_files)     <floorplan DEF file>
 # --------------------------------------------------------------------
 set vars(design)           "processing_system"
-set vars(netlist)          "$vars(data_root)/mult_seq.vh"
+set vars(netlist) 		   "$vars(data_root)/processing_system.v"
+
 
 #set vars(fp_file)    $vars(data_root)/dtmf_recvr_core.fp
 #set vars(cts_spec)  "$vars(data_root)/dtmf_recvr_core.ctstch"
