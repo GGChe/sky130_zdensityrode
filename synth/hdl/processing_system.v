f161fb1ef30dc015f646173a80c9e0f5cde71cb4
Gabriel Galeote Checa <ggaleotecheca@gmail.com>
2025-09-25 10:11:39 +0200

    update to 128 ch

--- synth/hdl/processing_system.v
+++ synth/hdl/processing_system.v
@@ -5,7 +5,7 @@
 `default_nettype none
 
 module processing_system #(
-    parameter integer NUM_UNITS  = 64,
+    parameter integer NUM_UNITS  = 128,
     parameter integer DATA_WIDTH = 16
 ) (
     input  wire                     clk,
